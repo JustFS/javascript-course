9c79341f7d0dd404689dcaf203399d33b6a0ec30
Julien Az <julien.azbrg@gmail.com>
2020-12-01 12:40:30 +0100

    corrected api

--- api/index.js
+++ api/index.js
@@ -3,15 +3,15 @@ const quote = document.getElementById("quote");
 const getQuote = () => {
   fetch("https://api.quotable.io/random")
     .then((res) => res.json())
-    .then((res) => {
-      quote.innerHTML = `${res.content}`;
+    .then((data) => {
+      quote.innerHTML = data.content;
     });
 
-  fetch("https://picsum.photos/1600/900")
-    .then((res) => res.url)
+  fetch('https://picsum.photos/1600/1000')
     .then((res) => {
-      document.getElementById("pic").innerHTML = `<img src=${res} />`;
-    });
+      document.getElementById('pic').innerHTML = 
+        `<img src=${res.url} />`
+    })
 };
 
 quote.addEventListener("click", () => getQuote());
